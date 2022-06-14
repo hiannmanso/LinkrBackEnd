94b538c9f2fe5a1078eee9c3107204e54516969f
Abner da Silva Tavares <98192830+AbnerTavares14@users.noreply.github.com>
2022-06-14 13:21:42 -0300

    Create linkr.sql

---
+++ linkr.sql
@@ -0,0 +1,39 @@
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    picture TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE sessions(
+    id SERIAL PRIMARY KEY,
+    "userID" INTEGER NOT NULL REFERENCES users(id),
+    token TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE posts(
+    id SERIAL PRIMARY KEY,
+    "userID" INTEGER NOT NULL REFERENCES users(id),
+    url TEXT UNIQUE NOT NULL,
+    description TEXT,
+    "createdAt" TIMESTAMP WITHOUT TIME ZONE NOW()
+);
+
+CREATE TABLE likes (
+    id SERIAL PRIMARY KEY,
+    "userID" INTEGER NOT NULL REFERENCES users(id),
+    "postID" INTEGER NOT NULL REFERENCES posts(id),
+    "createdAt" TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW()
+);
+
+CREATE TABLE hashtagsXposts(
+    id SERIAL PRIMARY KEY,
+    "postID" INTEGER NOT NULL REFERENCES posts(id),
+    "hashtagID" INTEGER REFERENCES hashtags(id)
+);
+
+CREATE TABLE hashtags(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
