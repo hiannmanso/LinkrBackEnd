8546a501e4aad1ae2e3322ec1c16acc0c1f9fc38
Hiann Luiz Manso <hiannmanso@gmail.com>
2022-06-14 11:38:22 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("Server running on port " + process.env.PORT);
-}));
+});
