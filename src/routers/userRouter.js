47fa9c37410e63a751100e25db75ce557a7fcf6a
samukaell <snoooydelta37@gmail.com>
2022-06-17 22:52:46 -0300

    Atualização da rota

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,8 +1,12 @@
 import { Router } from 'express'
 import { getUser } from '../controllers/userController.js'
+import { createUser } from '../controllers/userController.js'
+import userSchema from '../schemas/userSchema.js'
+import validateSchema from '../middlewares/schemaValidator.js'
 
 const userRouter = Router()
 
 userRouter.get('/user/:userID', getUser);
+userRouter.post('/signup', validateSchema(userSchema), createUser);
 
 export default userRouter
