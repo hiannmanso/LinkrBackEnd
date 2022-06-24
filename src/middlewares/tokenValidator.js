46a2951cb1990c8c47762b895b25c0c4586a2f4e
Hiann <hiannmanso@email.com>
2022-06-24 16:14:23 -0300

    fix bugs

--- src/middlewares/tokenValidator.js
+++ src/middlewares/tokenValidator.js
@@ -2,6 +2,7 @@ import db from '../db.js'
 
 export default async function tokenValidator(req, res, next) {
 	const { authorization } = req.headers
+
 	const token = authorization?.replace('Bearer ', '').trim()
 	if (!token) return res.status(422).send('Token not found.')
 	res.locals.token = token
